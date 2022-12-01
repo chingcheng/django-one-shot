77761bd1c8a93da9184bca680a7d4e2ad58ce08e
Ching Cheng <ching@chings-air.lan>
2022-12-01 11:37:47 -0800

    Feature 3 Complete

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # This tells Django how to convert our model into a string
+    # when we print() it, or when the admin displays it
+    def __str__(self):
+        return self.name
