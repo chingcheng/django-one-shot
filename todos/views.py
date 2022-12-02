c7c60ef7f4a0b950fca46ff87a15188e3e3bf867
Ching Cheng <ching@chings-air.lan>
2022-12-01 16:27:30 -0800

    Feature 9 Complete

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from todos.models import TodoList, TodoItem
+from todos.forms import TodoListForm
 
 # Create your views here.
 def todo_list_list(request):
@@ -16,3 +17,17 @@ def todo_list_detail(request, id):
         "todo_list": todo_list,
     }
     return render(request, "todos/detail.html", context)
+
+
+def todo_list_create(request):
+    if request.method == "POST":
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            list = form.save()
+            return redirect("todo_list_detail", id=list.id)
+    else:
+        form = TodoListForm()
+
+    context = {"form": form}
+
+    return render(request, "todos/create.html", context)
